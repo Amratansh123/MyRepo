fe8c67bdd764d5d3ddd96eba562af88c8d7e97bd
AmratanshT <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 12:20:18 +0530

    comment added

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,7 +3,7 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-		message = "hi";
+		message = "hi"; //java comment
 		return message;
 	}
 
