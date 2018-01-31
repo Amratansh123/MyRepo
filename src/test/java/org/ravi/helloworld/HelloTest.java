c3974c0af9dea3c0f8bfa00d16075ed9734ccafd
AmratanshT <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 15:14:10 +0530

    port change done for selenium

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -23,7 +23,7 @@ public class HelloTest {
 		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
+		driver.get("http://localhost:8086/Helloworld/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
